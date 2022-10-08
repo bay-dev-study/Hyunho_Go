236a168498457f26d769665c915de9abb278416a
신현호 <ho_study@sinhyeonhoui-MacBookAir-2.local>
2022-10-08 21:35:16 +0900

    #1.7 switch

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge > 18 {
+	switch koreanAge := age + 2; {
+	case koreanAge > 18:
 		return true
+	default:
+		return false
 	}
-	return false
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(18))
 }
